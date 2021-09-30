1a9fd48138352f485fc6ded9093f7e01d7d0151e
finuom <mouniftormos@gmail.com>
2021-09-30 13:12:30 -0400

    Correçao de bug

---
+++ app.js
@@ -0,0 +1,209 @@
+var express = require('express');
+var app = express();
+
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client/login.html');	
+});
+app.use('/client',express.static(__dirname + '/client'));
+app.use(express.static('public'));
+ 
+serv.listen(3000, '0.0.0.0', function() {
+    console.log('Listening to port:  ' + 3000);
+});
+console.log("Servidor iniciado.");
+
+
+var TAFFY = require( 'taffy' );
+
+//usuarios = TAFFY([{"usuario" : "abc", "senha": "123", "email":"a@gmail.com", "palavrasAcertadas" : 0}]);
+palavras = TAFFY([{"id" : 1, "nome" : "ALFACE", "dica" : "verdura"}, {"id" : 2, "nome" : "BETERRABA", "dica" : "vegetal"}, {"id" : 3, "nome" : "PATINETE", "dica" : "objeto esportivo"}, 
+				{"id" : 4, "nome" : "TOMATE", "dica" : "fruta"}, {"id" : 5, "nome" : "REPOLHO", "dica" : "vegetal"}, {"id" : 6, "nome" : "ORNITORRINCO", "dica" : "animal"},
+				{"id" : 7, "nome" : "BARATA", "dica" : "inseto"}, {"id" : 8, "nome" : "MICO LEAO DOURADO", "dica" : "animal"}, {"id" : 9, "nome" : "GAFANHOTO", "dica" : "inseto"},
+				{"id" : 10, "nome" : "TRAVESSEIRO", "dica" : "objeto de quarto"}, {"id" : 11, "nome" : "WINDOWS", "dica" : "sistema operacional"}, {"id" : 12, "nome" : "HIPOPOTAMO", "dica" : "animal"},
+				{"id" : 13, "nome" : "BATERIA", "dica" : "instrumento musical"}, {"id" : 14, "nome" : "BERINJELA", "dica" : "legume"}, {"id" : 15, "nome" : "HOVERBOARD", "dica" : "objeto esportivo"},
+				{"id" : 16, "nome" : "LEITE", "dica" : "bebida"}, {"id" : 17, "nome" : "PLUMA", "dica" : "objeto (muito leve)"}, {"id" : 18, "nome" : "PASSARO", "dica" : "animal"},
+				{"id" : 19, "nome" : "UVA", "dica" : "fruta"}, {"id" : 20, "nome" : "GUARDA ROUPAS", "dica" : "objeto de quarto"}, {"id" : 21, "nome" : "POMBO", "dica" : "animal"}, 
+				{"id" : 22, "nome" : "CACHECOL", "dica" : "vestimenta"}, {"id" : 23, "nome" : "FIGADO", "dica" : "órgão do corpo humano"}, {"id" : 24, "nome" : "CANADA", "dica" : "país"},
+				{"id" : 25, "nome" : "SUCO DE TANGERINA", "dica" : "bebida"}, {"id" : 26, "nome" : "TOMADA", "dica" : "objeto de parede"}, {"id" : 27, "nome" : "COMPUTADOR", "dica" : "objeto eletrônico"},
+				{"id" : 28, "nome" : "SAPATO", "dica" : "calçado"}, {"id" : 29, "nome" : "MILHO VERDE", "dica" : "legume"}, {"id" : 30, "nome" : "ESTILETE", "dica" : "arma branca"},
+				{"id" : 31, "nome" : "PIANO", "dica" : "instrumento musical"}, {"id" : 32, "nome" : "TACO DE BEISEBOL", "dica" : "equipamento esportivo"}, {"id" : 33, "nome" : "CINZA", "dica" : "cor"},
+				{"id" : 34, "nome" : "MARROM", "dica" : "cor"}, {"id" : 35, "nome" : "ESPADA", "dica" : "arma branca"}, {"id" : 36, "nome" : "MACHADO", "dica" : "arma branca"}]);
+
+usuarios = TAFFY();
+usuarios.store('./scratch');
+
+console.log("\nTabela de usuarios: ");
+console.log(usuarios().get());
+console.log("\nTabela de Palavras: ");
+console.log(palavras().get());
+ 
+// Lista de sockets
+var SOCKET_LIST = {};
+// Lista de jogadores
+var PLAYER_LIST = {};
+ 
+// Variável que armazena as informações do jogador
+var Player = function(id){
+    var self = {
+        id: id,    
+		numero: 0
+    }
+    return self;
+}
+
+var io = require('socket.io')(serv,{});
+// Variável aleatoria que é usada para escolher o ID de alguma palavra na lista de palavras
+var aleatorio = Math.floor(Math.random() * 35) + 1;
+console.log("Palavra(ID): " + aleatorio);
+// Variáveis utilizadas para o controle de jogadores
+var aux = 0;
+var aux2 = 0;
+var auxJogadores = [];
+var id = 0;
+// Escolhe uma palavra com base em um número aleatório
+var palavraEscolhida = palavras({id:aleatorio}).first().nome;	
+// Guarda a dica da palavra escolhida
+var dica = palavras({id:aleatorio}).first().dica; 
+// Todas as letras que foram escolhidas na jogada ficam guardadas nessa estrutura 
+var letrasEscolhidas = [];
+
+function proximoJogador(){
+				console.log("1. Aux2:");
+				console.log(aux2);
+				
+		if(aux2 >= Object.keys(PLAYER_LIST).length - 1)
+			aux2 = 0;
+		else
+			aux2++;
+			
+				console.log("2. Aux2:");
+				console.log(aux2);
+				
+		console.log('Jogadores: ', auxJogadores); 
+				
+		for(let i in SOCKET_LIST){
+			for(let j in PLAYER_LIST){
+				if(i == j && PLAYER_LIST[j].numero == auxJogadores[aux2]){
+					SOCKET_LIST[i].emit('suavez');
+					console.log('Vez do jogador ' + auxJogadores[aux2]);
+				}
+			}
+		}
+}
+
+io.sockets.on('connection', function(socket){	
+    socket.id = id; 					
+	id++;									
+    SOCKET_LIST[socket.id] = socket;	
+    console.log('Jogador ' + socket.id + ' conectado'); 
+    var player = Player(socket.id);	
+    player.numero = aux;
+	auxJogadores.push(aux);
+	aux++;
+    PLAYER_LIST[socket.id] = player;   
+   
+    socket.on('escolher', function(letra){
+        console.log('letra escolhida:' + letra);
+        for(var i in SOCKET_LIST){
+			var socket = SOCKET_LIST[i];
+            var letra = letra;
+            socket.emit('novaescolha',letra);
+			letrasEscolhidas.push(letra);
+        }		
+				//console.log("Tamanho da lista de jogadores:");
+				//console.log(Object.keys(PLAYER_LIST).length);
+		proximoJogador();		
+    });
+	
+	socket.on('chutarPalavra', function(dados){		
+		var palavraEscolhidaAux = palavraEscolhida;
+		if(dados.palavra === palavraEscolhida){
+			var aleatorio = Math.floor(Math.random() * 35) + 1;
+			console.log("Palavra(ID): " + aleatorio);
+			palavraEscolhida = palavras({id:aleatorio}).first().nome;				
+			dica = palavras({id:aleatorio}).first().dica;			
+			var u = dados.usuario;			
+			if(usuarios().filter({usuario: dados.usuario}).count() > 0){
+				usuarios().filter({usuario: dados.usuario}).update({palavrasAcertadas: dados.palavras_acertadas + 1});								
+			}			
+			for(let i in SOCKET_LIST){
+				var s = SOCKET_LIST[i];			
+				if(dados.palavra === palavraEscolhidaAux){					
+					s.emit('novochutepalavra', palavraEscolhidaAux);
+					s.emit('adicionarPalavra', dados.palavra);
+					s.emit('fim');
+					s.emit('permitir', 'usuario');											
+					s.emit('inicia', {palavraEscolhida: palavraEscolhida, dica: dica});
+					s.emit('atualizarplacar', {usuario: dados.usuario, palavras_acertadas: dados.palavras_acertadas + 1});
+					letrasEscolhidas = [];
+				}else{
+					s.emit('adicionarPalavra', dados.palavra);
+				}			
+			}
+			socket.emit('ganhou');
+			socket.emit('suavez');						
+		}
+	});
+	
+	socket.on('logar', function(dados){
+		var u = dados.usuario;
+		var s = dados.senha;
+		if(usuarios().filter({usuario: u, senha: s}).count() > 0){
+			socket.emit('permitir', u);
+			socket.emit('inicia', {palavraEscolhida: palavraEscolhida, dica: dica});
+			socket.emit('letrasescolhidas', letrasEscolhidas);
+			if(PLAYER_LIST[socket.id].numero == auxJogadores[aux2] || Object.keys(PLAYER_LIST).length == 1){
+				socket.emit('suavez');
+				console.log('Vez do jogador ' + PLAYER_LIST[socket.id].numero);
+			}			
+		}
+		else
+			socket.emit('negar');
+	});
+	
+	socket.on('cadastrar', function(dados){
+		var u = dados.usuario;
+		var s = dados.senha;
+		var e = dados.email;
+		if(usuarios().filter({usuario: u}).count() == 0){
+			usuarios.insert({usuario:u, senha:s, email:e, palavrasAcertadas: 0});
+			socket.emit('cadastrado');
+			console.log(usuarios().get());
+		}
+		else
+			socket.emit('negar');
+	});
+    // O socket cliente automaticamente emite essa mensagem, o servidor deleta os dados do cliente relacionados ao socket/jogador, ajusta os jogadores, e passa para o próximo jogador caso o jogador tenha saido no seu turno
+	socket.on('disconnect',function(){   
+		var a = false;
+		for(let i in auxJogadores){
+			if(auxJogadores[i] === PLAYER_LIST[socket.id].numero){
+				if(aux2 == Object.keys(PLAYER_LIST).length - 1){
+					aux2++;
+					proximoJogador();
+					auxJogadores.splice(i, 1); //Retira o número do jogador da lista de participantes	
+					a = true;
+					break;							
+				}else{
+					proximoJogador();
+					auxJogadores.splice(i, 1); //Retira o número do jogador da lista de participantes	
+				}				
+			}
+		}
+		
+		console.log(auxJogadores); 
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+		console.log('Jogador ' + socket.id + ' desconectado');
+   });
+});
+ 
+setInterval(function(){
+    var pack = [];
+    for(let i in PLAYER_LIST){
+        var player = PLAYER_LIST[i];   
+    }            
+},1000/25);
+ 
---
+++ public/js/loginScript.js
@@ -0,0 +1,331 @@
+﻿var socket = io();
+var nome_de_usuario;
+var palavrasAcertadas = 0;
+login();
+
+function login(){
+	var loginBox = document.querySelector('.loginbox');
+	loginBox.innerHTML = "";
+	
+	var h1 = document.createElement('h1');
+	h1.innerHTML = "Faça o Login";
+	loginBox.appendChild(h1);
+	
+	var u = document.createElement('p');
+	u.innerHTML = "Usuário";
+	aviso = document.createElement('span');
+	aviso.innerHTML = " *Usuário ou Senha incorretos";
+	aviso.classList.add('aviso');
+	aviso.classList.add('hidden');
+	u.appendChild(aviso);
+	loginBox.appendChild(u);
+	
+	var inpU = document.createElement('INPUT');
+	inpU.id = 'usuario';
+	inpU.setAttribute("type", "text");
+	inpU.setAttribute("placeholder", "Insira seu Nome");
+	loginBox.appendChild(inpU);
+	
+	var s = document.createElement('p');
+	s.innerHTML = "Senha";	
+	loginBox.appendChild(s);
+	
+	var inpS = document.createElement('INPUT');
+	inpS.id = 'senha';
+	inpS.setAttribute("type", "password");
+	inpS.setAttribute("placeholder", "Insira sua Senha");
+	loginBox.appendChild(inpS);
+	
+	var btnLogar = document.createElement('button');
+	btnLogar.innerHTML = 'ENTRAR';
+	btnLogar.id = 'btnLogar';
+	btnLogar.setAttribute("onclick", "logar()");
+	loginBox.appendChild(btnLogar);
+	
+	var btnCadastrar = document.createElement('button');
+	btnCadastrar.innerHTML = 'CADASTRAR NOVA CONTA';
+	btnCadastrar.id = 'btnCadastrar';
+	btnCadastrar.setAttribute("onclick", "cadastro()");
+	loginBox.appendChild(btnCadastrar);		
+}
+
+function logar() {	
+	usuario = document.getElementById("usuario");
+	var u = usuario.value;
+	senha = document.getElementById("senha");
+	var s = senha.value;
+	if(u === "" || s === ""){
+		alert("Por favor, informe todas as informações");
+		return;
+	}	
+	socket.emit('logar', {usuario: u, senha: s});	
+	nome_de_usuario = u;
+}
+
+socket.on('negar', function(){
+	const aviso = document.querySelector('.aviso');
+	aviso.classList.remove('hidden');
+});
+
+function cadastro(){
+	loginBox = document.querySelector('.loginbox');
+	var h1 = loginBox.querySelector('h1');
+	h1.innerHTML = "Faça o Cadastro";
+	loginBox.removeChild(loginBox.querySelector('#btnLogar'));
+	loginBox.removeChild(loginBox.querySelector('#btnCadastrar'));
+	aviso = document.querySelector('.aviso');
+	aviso.classList.add('hidden');
+	aviso.innerHTML = " *Usuário já existe!";
+	var p = document.createElement('p');
+	p.innerHTML = 'E-Mail'
+	loginBox.appendChild(p);
+	var inp = document.createElement('INPUT');
+	inp.id = 'email';
+	inp.setAttribute("type", "text");
+	inp.setAttribute("placeholder", "Insira seu E-Mail");
+	loginBox.appendChild(inp);
+	var btnCadastrar = document.createElement('button');
+	btnCadastrar.innerHTML = 'CADASTRAR';
+	btnCadastrar.id = 'btnCadastrar';
+	btnCadastrar.setAttribute("onclick", "cadastrar()");
+	loginBox.appendChild(btnCadastrar);
+}
+
+function cadastrar(){
+	usuario = document.getElementById("usuario");
+	var u = usuario.value;
+	senha = document.getElementById("senha");
+	var s = senha.value;
+	email = document.getElementById('email');
+	var e = email.value;
+	if(u === "" || s === "" || e === ""){
+		alert("Por favor, informe todas as informações");
+		return;
+	}	
+	socket.emit('cadastrar', {usuario: u, senha: s, email: e});		
+}
+
+socket.on('cadastrado', function(){
+	alert('Usuário cadastrado!');
+	login();
+});
+
+socket.on('permitir', function(){
+	body = document.querySelector('body');
+	d1 = document.querySelector('.d1');
+	if(d1 != null){
+		var c01 = document.querySelector('.c01');	
+		c01.innerHTML = '';		
+	}
+	else{
+		body.innerHTML = '';
+		d1 = document.createElement('div');
+		d1.className = 'd1';
+		
+		var c01 = document.createElement('div');	
+		c01.className = 'c01';
+		d1.appendChild(c01);
+		
+		var c04ec05 = document.createElement('div');
+		c04ec05.className = 'c04ec05';
+		
+		var c04 = document.createElement('div');	
+		c04.className = 'c04';
+		var container = document.createElement('div');
+		c04.appendChild(container);
+		socket.on('adicionarPalavra', function(palavra){
+			var p = document.createElement('p');
+			p.innerHTML = palavra;
+			container.appendChild(p);	
+			if(container.childElementCount > 4){
+				container.removeChild(container.querySelector('p'));
+			}
+		});		
+		var inp = document.createElement('INPUT');
+		inp.setAttribute("type", "text");
+		inp.setAttribute("style", "text-transform:uppercase");
+		inp.addEventListener("keyup", function(event) {
+			if (event.key === "Enter") {
+				var maiusculo = inp.value.toUpperCase();
+				inp.value = '';
+				socket.emit('chutarPalavra', {palavra: maiusculo, usuario:nome_de_usuario, palavras_acertadas: palavrasAcertadas}); // emite uma mensagem junto com a letra			
+			}
+		});
+		c04.appendChild(inp);
+		c04ec05.appendChild(c04);
+		
+		var c05 = document.createElement('div');	
+		c05.className = 'c05';
+		var nome = document.createElement('p');
+		nome.id = 'nome';
+		nome.innerHTML = nome_de_usuario;
+		c05.appendChild(nome);
+		var pAcertadas = document.createElement('p');
+		pAcertadas.id = 'pAcertadas';
+		pAcertadas.innerHTML = 'Palavras Acertadas: ' + palavrasAcertadas;
+		c05.appendChild(pAcertadas);
+		var vez = document.createElement('p');
+		vez.id = 'vez';
+		vez.innerHTML = '>>SUA VEZ<<';
+		vez.style.visibility = 'hidden';
+		c05.appendChild(vez);
+		c04ec05.appendChild(c05);
+		
+		d1.appendChild(c04ec05);
+		
+		var c03 = document.createElement('div');	
+		c03.className = 'c03';
+		//Cria várias divs com caracteres de A até Z, usando valores da tabela ASCII
+		for(i = 65; i < 91; i++){
+			var div = document.createElement('div');
+			div.innerHTML = String.fromCharCode(i);
+			div.setAttribute("onclick","verifica(this)");		
+			c03.appendChild(div);
+		}
+		d1.appendChild(c03);
+			
+		body.appendChild(d1);
+	}					
+	
+	if(document.querySelector('#placar') == null){
+		var d2 = document.createElement('div');
+		d2.className = 'd2';
+		
+		var placar = document.createElement('div');
+		placar.id = 'placar';
+		
+		var placarh1 = document.createElement('h1');
+		placarh1.innerHTML = 'Placar de Jogadores';
+		placar.appendChild(placarh1);	
+		
+		var placarh2 = document.createElement('h2');
+		placarh2.innerHTML = 'Palavras Acertadas:';
+		placar.appendChild(placarh2);
+		
+		d2.appendChild(placar);	
+		body.appendChild(d2);
+	}			
+});		
+
+socket.on('atualizarplacar', function(dados){
+	var childPs = document.getElementById('placar').getElementsByTagName('p');
+	var childSpans = document.getElementById('placar').getElementsByTagName('span');
+	for(let i = 0; i < childPs.length; i++){
+		var childP = childPs[i];
+		console.log(childP.innerHTML);
+		if(childP.innerHTML === dados.usuario){
+			childSpans[i].innerHTML = dados.palavras_acertadas;				
+			return;
+		}
+	}	
+	var placar = document.getElementById('placar');
+	
+	var placarJogador = document.createElement('p');
+	placarJogador.id = 'placarJogador';
+	placarJogador.innerHTML = dados.usuario;
+	placar.appendChild(placarJogador);
+	
+	var placarPalavras = document.createElement('span');
+	placarPalavras.id = 'placarPalavras';
+	placarPalavras.innerHTML = dados.palavras_acertadas;
+	placar.appendChild(placarPalavras);
+});
+
+var palavra;
+var minhavez = false;
+
+socket.on('suavez', function(){
+	var vez = document.getElementById('vez');
+	alert('agora é sua vez!');
+	if(vez != undefined)
+		vez.style.visibility = 'visible';
+	minhavez = true;
+});
+
+socket.on('inicia',function inicia(dados){				
+	palavra = dados.palavraEscolhida;
+	dica = dados.dica;
+	console.log('dica: ' + dica);
+	var container = document.querySelector('.c01');
+	for(let i = 0; i < palavra.length; i++){		
+		
+		var under = document.createElement('div');
+				
+		if(palavra[i] == ' '){
+			under.classList.add('espaco');
+		}else{
+			under.classList.add('underline');
+		}
+		under.id = i;		
+		container.appendChild(under);		
+	}
+	var pDica = document.createElement('p');
+	pDica.id = 'dica';
+	pDica.innerHTML = 'dica: ' + dica;
+	container.appendChild(pDica);
+});
+
+socket.on('letrasescolhidas', function (letrasEscolhidas){
+	for (let i = 0; i < palavra.length; i++){
+		for (let j = 0; j < letrasEscolhidas.length; j++){
+			if(letrasEscolhidas[j] == palavra[i] && document.getElementById(i).innerHTML === ""){					
+				document.getElementById(i).innerHTML = letrasEscolhidas[j];				
+			}		
+		}
+	}
+});
+
+function verifica(elem){			
+	if(minhavez == true){ 
+		var letra = elem.innerHTML; 
+		socket.emit('escolher', letra); 
+	}
+	else{
+		alert('Espere a sua vez');
+	}
+}
+
+socket.on('novaescolha', function(letra){
+	var acertosEscolha = 0;
+	if(palavra != undefined)
+		for (i = 0; i < palavra.length; i++){					
+			if(letra == palavra[i] && document.getElementById(i).innerHTML === ""){					
+				document.getElementById(i).innerHTML = letra;
+				acertosEscolha++;
+			}		
+		}
+	if(minhavez == true){
+		if(acertosEscolha > 0)
+			alert('Acertou: ' + acertosEscolha);
+		else
+			alert('Errou!');
+		minhavez = false;
+		vez.style.visibility = 'hidden';
+	}	
+});
+
+socket.on('novochutepalavra', function(chutePalavra){
+	for (i = 0; i < palavra.length; i++){	
+		for(j = 0; j < chutePalavra.length; j++){
+			if(chutePalavra[j] === palavra[i] && document.getElementById(i).innerHTML === ""){					
+				document.getElementById(i).innerHTML = chutePalavra[j];				
+			}
+		}
+	}
+	if(minhavez == true){
+		minhavez = false;
+		vez.style.visibility = 'hidden';
+	}	
+});
+
+socket.on('ganhou', function(){			
+	alert('Você Ganhou!');
+	palavrasAcertadas++;
+	document.getElementById('pAcertadas').innerHTML = 'Palavras Acertadas: ' + palavrasAcertadas;
+});
+
+socket.on('fim', function(){			
+	alert('Fim de Jogo!');
+});
+
+	
